795a8ee0b59e598d9a8511eb5eaf633ccd6eb692
enoch-bolarinwa <enoch-bolarinwa@>
2025-03-22 22:40:16 +0100

    frontend

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongodb = require('./data/database')
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
-app.use('/', require('./route'))
+app.use('/', require('./routes'))
 
 mongodb.initDb((err) => {
     if(err) {
