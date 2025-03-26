7d0a3495eb1979377b90d53ad4137c588a689586
enoch-bolarinwa <enoch-bolarinwa@>
2025-03-26 10:29:13 +0100

    commit all

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,22 @@
+const routes = require('express').Router();
+const temple = require('./temple');
+
+routes.use('/temples', temple);
+routes.use(
+  '/',
+  (docData = (req, res) => {
+    let docData = {
+      documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
+    };
+    res.send(docData);
+  })
+);
+
+
+
+
+module.exports = routes;
+
 const router = require('express').Router();
 //router.get('/',(req, res) =>{res.send('Hello World');});
 
